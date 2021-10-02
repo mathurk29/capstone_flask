8ffbd0139a5319b80efc057b61fc8cbdfe97e520
Kshitij Mathur <mathurk29@gmail.com>
2021-10-02 18:09:10 +0530

    web forms with wtf

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, app, render_template
+
+# wtf is not flask specific - the following lib implements it
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField 
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField  
+from wtforms.validators import DataRequired 
 
 # Create a Flask instance
 application = Flask(__name__)
+application.config['SECRET_KEY'] = "SOMEKEY"
+
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField('Enter your name', validators=[DataRequired()])
+    submit = SubmitField('Enter button')
+
 
 #Create a route decorator
 @application.route('/')
@@ -24,3 +33,15 @@ def user(username):
 @application.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html")
+
+
+# Create Name Page
+@application.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('name.html', name=name, form=form)
+
