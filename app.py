f1eae3c42a8be008c4913b4a670ac5014ca698dc
Kshitij Mathur <mathurk29@gmail.com>
2021-10-13 19:55:51 +0530

    whitespaces

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, app, render_template, flash
-
 # wtf is not flask specific - the following lib implements it
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField 
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask import redirect, url_for, request
 # Create a Flask instance
 application = Flask(__name__)
 application.config['SECRET_KEY'] = "SOMEKEY" # for CRF tokens
@@ -17,17 +15,17 @@ class NamerForm(FlaskForm):
 
 #Create a route decorator
 @application.route('/')
-
 # def index():
 #     return "<h1>Hello World!</h1>"
-
 def index():
     return render_template("index.html")
 
 # localhost:5000/user/john
+
+
 @application.route("/user/<username>")
 def user(username):
-    return render_template('user.html', username=username) 
+    return render_template('user.html', username=username)
 
 
 @application.errorhandler(404)
@@ -42,7 +40,6 @@ def name():
     form = NamerForm()
     if form.validate_on_submit():
         name = form.name.data
-        form.name.data=''
+        form.name.data = ''
         flash("Form sumitted successfully.")
     return render_template('name.html', name=name, form=form)
-
